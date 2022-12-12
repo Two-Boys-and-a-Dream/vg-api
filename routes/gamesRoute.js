6c84787e498d36ead7823bf0a512b1c446b95e18
Winston <WLHDeveloper@gmail.com>
2022-12-11 19:01:21 -0500

    -router for games routes

---
+++ routes/gamesRoute.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const controller = require('../controllers/gamesController')
+
+router.get('/new', controller.newGames);
+router.get('/upcoming', controller.upcomingGames);
+router.get('/popular', controller.popularGames);
