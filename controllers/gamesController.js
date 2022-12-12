b3503b8c300c82310ca1726d25f0165e7611b305
Winston <WLHDeveloper@gmail.com>
2022-12-12 14:36:23 -0500

    -added reusable axios post to all games routes

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -3,19 +3,35 @@ const reusableAxiosPost = require("../igdbHelper");
 module.exports = {
   newGames: async (req, res) => {
     try {
-      const { data } = await reusableAxiosPost();
+      const { data } = await reusableAxiosPost("new");
       res.status(200);
       res.json(data);
     } catch (e) {
       console.log(e);
       res.status(400);
-      res.end();
+      res.send("happend in new games route");
     }
   },
   upcomingGames: async (req, res) => {
-    res.send("yup");
+    try {
+      const { data } = await reusableAxiosPost("upcoming");
+      res.status(200);
+      res.send(data);
+    } catch (e) {
+      console.log(e);
+      res.status(400);
+      res.send("happend in upcoming games route");
+    }
   },
   popularGames: async (req, res) => {
-    res.send("yup");
+    try {
+      const { data } = await reusableAxiosPost("popular");
+      res.status(200);
+      res.send(data);
+    } catch (e) {
+      console.log(e);
+      res.status(400);
+      res.send("happend in popular games route");
+    }
   },
 };
