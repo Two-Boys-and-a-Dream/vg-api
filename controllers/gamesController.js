083324967a62d2461f5b21d0e4806241c73d1ce2
Winston <WLHDeveloper@gmail.com>
2022-12-12 15:51:58 -0500

    -formatted all files

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,37 +1,37 @@
-const Post = require("../igdbHelper");
+const Post = require('../igdbHelper')
 
 module.exports = {
-  newGames: async (req, res) => {
-    try {
-      const { data } = await Post("new");
-      res.status(200);
-      res.json(data);
-    } catch (e) {
-      console.log(e);
-      res.status(400);
-      res.send("happend in new games route");
-    }
-  },
-  upcomingGames: async (req, res) => {
-    try {
-      const { data } = await Post("upcoming");
-      res.status(200);
-      res.send(data);
-    } catch (e) {
-      console.log(e);
-      res.status(400);
-      res.send("happend in upcoming games route");
-    }
-  },
-  popularGames: async (req, res) => {
-    try {
-      const { data } = await Post("popular");
-      res.status(200);
-      res.send(data);
-    } catch (e) {
-      console.log(e);
-      res.status(400);
-      res.send("happend in popular games route");
-    }
-  },
-};
+    newGames: async (req, res) => {
+        try {
+            const { data } = await Post('new')
+            res.status(200)
+            res.json(data)
+        } catch (e) {
+            console.log(e)
+            res.status(400)
+            res.send('happend in new games route')
+        }
+    },
+    upcomingGames: async (req, res) => {
+        try {
+            const { data } = await Post('upcoming')
+            res.status(200)
+            res.send(data)
+        } catch (e) {
+            console.log(e)
+            res.status(400)
+            res.send('happend in upcoming games route')
+        }
+    },
+    popularGames: async (req, res) => {
+        try {
+            const { data } = await Post('popular')
+            res.status(200)
+            res.send(data)
+        } catch (e) {
+            console.log(e)
+            res.status(400)
+            res.send('happend in popular games route')
+        }
+    },
+}
