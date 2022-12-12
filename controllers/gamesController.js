69c5f65607d705a2d7465326ed8431d565cfe220
Winston <WLHDeveloper@gmail.com>
2022-12-11 20:06:45 -0500

    -set up getting access token function

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,7 +1,22 @@
+const axios = require("axios");
+require('dotenv').config();
+
+const getAccessToken = async()=>{
+    try{
+        const {CLIENT_ID, CLIENT_SECRET} = process.env;
+        const url = `https://id.twitch.tv/oauth2/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials`
+        const response = await axios.post(url);
+        return response.data.access_token;
+    }catch (e) {
+        console.log(e);
+    }
+}
+
 module.exports = {
+    newGames: async (req, res)=>{
+        const token = await getAccessToken();
 
-    newGames: (req, res)=>{
-
+        res.end();
     },
     upcomingGames: (req, res)=>{
 
