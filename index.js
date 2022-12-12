3171de4c0ff146d26cd76b00b806bc1edb9e3aee
Winston <WLHDeveloper@gmail.com>
2022-12-11 19:25:29 -0500

    -games route added to app use

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = require('express')();
+const gamesRouter = require('./routes/gamesRoute');
 const PORT = 3001
+
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use('/games', gamesRouter);
 
 app.listen(PORT, ()=>console.log(`Listening on port ${PORT}`))
