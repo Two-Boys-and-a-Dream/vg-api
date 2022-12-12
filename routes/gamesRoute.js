02506e887c6314dd84a1e4e0df9c38f2cc89efac
Winston <WLHDeveloper@gmail.com>
2022-12-11 19:22:25 -0500

    -exported games routes

--- routes/gamesRoute.js
+++ routes/gamesRoute.js
@@ -4,3 +4,5 @@ const controller = require('../controllers/gamesController')
 router.get('/new', controller.newGames);
 router.get('/upcoming', controller.upcomingGames);
 router.get('/popular', controller.popularGames);
+
+module.exports = router;
