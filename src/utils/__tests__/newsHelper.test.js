8a5cbbdcc5d3d31fc7f2e8bc2cdf0de8d1c9246d
Raymond Cox <raymondc1911@gmail.com>
2022-12-31 12:37:18 -0500

    Added missing tests

--- src/utils/__tests__/newsHelper.test.js
+++ src/utils/__tests__/newsHelper.test.js
@@ -1,7 +1,15 @@
 const { default: axios } = require('axios')
 const NewsHelper = require('../newsHelper')
+const { routeHandler } = require('../newsHelper')
 
 const axiosResponse = { data: [{ id: '1' }, { id: '2' }] }
+const req = {}
+const res = {
+    status: jest.fn(),
+    json: jest.fn(),
+    send: jest.fn(),
+}
+
 beforeEach(() => {
     jest.resetAllMocks()
     axios.get.mockResolvedValue(axiosResponse)
@@ -16,5 +24,19 @@ describe('newsHelper class', () => {
 
             expect(results).toEqual(axiosResponse.data)
         })
+    })
+})
+
+describe('routeHandler', () => {
+    it('handles success', async () => {
+        await routeHandler(req, res)
+        expect(res.status).toHaveBeenCalledWith(200)
+        expect(res.json).toHaveBeenCalledWith(axiosResponse.data)
+    })
+    it('handles error', async () => {
+        axios.get.mockRejectedValue({})
+        await routeHandler(req, res)
+        expect(res.status).toHaveBeenCalledWith(400)
+        expect(res.send).toHaveBeenCalledWith({})
     })
 })
