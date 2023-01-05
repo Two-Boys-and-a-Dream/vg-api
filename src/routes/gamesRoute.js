dbe7e3316a8418b94b9733f34dd7faa1bd3393ef
Raymond Cox <raymondc1911@gmail.com>
2023-01-05 09:17:49 -0500

    Added query single game by id route

--- src/routes/gamesRoute.js
+++ src/routes/gamesRoute.js
@@ -4,5 +4,6 @@ const { routeHandler } = require('../utils/igdbHelper')
 router.get('/new', routeHandler)
 router.get('/upcoming', routeHandler)
 router.get('/popular', routeHandler)
+router.get('/single', routeHandler)
 
 module.exports = router
--- src/utils/queries.js
+++ src/utils/queries.js
@@ -3,6 +3,11 @@ const { oneWeekAgoUnix, nowUnix, oneMonthAgoUnix } = require('./times')
 const FIELDS = {
     // Includes base information we plan to use for each and every game across the site.
     game: 'name, slug, total_rating, total_rating_count, cover.image_id',
+    // Full game information for a single game.
+    game_extended: 'follows, genres.name, genres.slug, summary, websites.url',
+    // Same information as "game", but nested.
+    similar_games:
+        'similar_games.name, similar_games.slug, similar_games.total_rating, similar_games.total_rating_count, similar_games.cover.image_id',
     // platform by itself is unused currently, but could serve as useful in the future.
     platform:
         'platforms.abbreviation, platforms.slug, platforms.platform_logo.image_id',
@@ -10,6 +15,11 @@ const FIELDS = {
     // to remakes/rereleases.
     release_dates:
         'release_dates.date, release_dates.human, release_dates.platform.abbreviation, release_dates.platform.slug, release_dates.platform.platform_logo.image_id',
+    screenshots: 'screenshots.image_id, screenshots.height, screenshots.width',
+    videos: 'videos.name, videos.video_id',
+    game_engines: 'game_engines.name, game_engines.slug, game_engines.url',
+    involved_companies:
+        'involved_companies.company.name, involved_companies.company.slug, involved_companies.company.websites.url, involved_companies.developer, involved_companies.publisher, involved_companies.porting, involved_companies.supporting',
 }
 
 const WHERE = {
